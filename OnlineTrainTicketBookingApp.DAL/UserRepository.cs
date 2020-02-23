e34382856b4abba2998c147dfd084a51a6cc06a8
ELCOT <ELCOT@LENOVO>
2020-02-22 20:17:05 -0800

    Binding

--- OnlineTrainTicketBookingApp.DAL/UserRepository.cs
+++ OnlineTrainTicketBookingApp.DAL/UserRepository.cs
@@ -2,6 +2,8 @@
 using System.Data;
 using System.Data.SqlClient;
 using OnlineTrainTicketBookingApp.DAL;
+using System.Collections.Generic;
+
 
 namespace OnlineTrainTicketBooking
 { 
@@ -9,65 +11,79 @@ namespace OnlineTrainTicketBooking
     {
         //protected static List<User> userList = new List<User>();        //A static user list to store user details 
         //SqlConnection sqlConnection = DBUtils.GetDBConnection();
-        SqlConnection sqlConnection = Connectivity.EstablishConnection();
-        public int RegisterDetail(User user)            //Method to make user to register with the account
+        //SqlConnection sqlConnection = Connectivity.EstablishConnection();
+        //public int RegisterDetail(User user)            //Method to make user to register with the account
+        //{
+        //    using (SqlCommand sqlCommand = new SqlCommand("USER_ADMIN_Registration", sqlConnection))
+        //    {
+        //        sqlCommand.CommandType = CommandType.StoredProcedure;
+        //        sqlConnection.Open();
+        //        sqlCommand.Parameters.Add(new SqlParameter("@firstname", user.FirstName));
+        //        sqlCommand.Parameters.Add(new SqlParameter("@lastname", user.LastName));
+        //        sqlCommand.Parameters.Add(new SqlParameter("@age", user.Age));
+        //        sqlCommand.Parameters.Add(new SqlParameter("@sex", user.Sex));
+        //        sqlCommand.Parameters.Add(new SqlParameter("@email", user.Email));
+        //        sqlCommand.Parameters.Add(new SqlParameter("@mobileno", user.MobileNum));
+        //        sqlCommand.Parameters.Add(new SqlParameter("@password", user.Password));
+        //        sqlCommand.Parameters.Add(new SqlParameter("@role", user.Status));
+        //        int retRows = sqlCommand.ExecuteNonQuery();
+        //        sqlCommand.Dispose();
+        //        sqlConnection.Close();
+        //        return retRows;
+        //    }            
+
+        //}
+
+        //public short VerifyWithDB(string userName, string passWord)
+        //{
+
+        //    using (SqlCommand sqlCommand = new SqlCommand("spCHECKDB", sqlConnection))
+        //    {
+        //        SqlParameter sqlParameter = new SqlParameter();
+        //        sqlCommand.CommandType = CommandType.StoredProcedure;
+        //        sqlCommand.Parameters.AddWithValue("@firstname", userName);
+        //        sqlCommand.Parameters.AddWithValue("@password", passWord);
+        //        sqlCommand.Parameters.Add("@id", SqlDbType.Int, 3);
+        //        sqlCommand.Parameters["@id"].Direction = ParameterDirection.Output;
+        //        sqlConnection.Open();
+        //        sqlCommand.ExecuteNonQuery();
+        //        short ID = Convert.ToInt16(sqlCommand.Parameters["@id"].Value);
+        //        sqlConnection.Close();
+        //        return ID;
+        //    }
+        //}
+
+        //public string FetchRole(int ID)
+        //{
+        //    using (SqlCommand sqlCommand = new SqlCommand("spFETCHROLE", sqlConnection))
+        //    {
+        //        SqlParameter sqlParameter = new SqlParameter();
+        //        sqlCommand.CommandType = CommandType.StoredProcedure;
+        //        sqlCommand.Parameters.AddWithValue("@id", ID);
+        //        sqlCommand.Parameters.Add("@role", SqlDbType.VarChar, 6);
+        //        sqlCommand.Parameters["@role"].Direction = ParameterDirection.Output;
+        //        sqlConnection.Open();
+        //        sqlCommand.ExecuteNonQuery();
+        //        string role = Convert.ToString(sqlCommand.Parameters["@role"].Value);
+        //        sqlConnection.Close();
+        //        return role;
+        //    }
+        //}
+
+
+        static List<User> userDetails = new List<User>();
+        static UserRepository()
         {
-            using (SqlCommand sqlCommand = new SqlCommand("USER_ADMIN_Registration", sqlConnection))
-            {
-                sqlCommand.CommandType = CommandType.StoredProcedure;
-                sqlConnection.Open();
-                sqlCommand.Parameters.Add(new SqlParameter("@firstname", user.firstName));
-                sqlCommand.Parameters.Add(new SqlParameter("@lastname", user.lastName));
-                sqlCommand.Parameters.Add(new SqlParameter("@age", user.age));
-                sqlCommand.Parameters.Add(new SqlParameter("@sex", user.sex));
-                sqlCommand.Parameters.Add(new SqlParameter("@email", user.email));
-                sqlCommand.Parameters.Add(new SqlParameter("@mobileno", user.mobileNum));
-                sqlCommand.Parameters.Add(new SqlParameter("@password", user.password));
-                sqlCommand.Parameters.Add(new SqlParameter("@role", user.role));
-                int retRows = sqlCommand.ExecuteNonQuery();
-                sqlCommand.Dispose();
-                sqlConnection.Close();
-                return retRows;
-            }            
-            
+            userDetails.Add(new User { FirstName = "Monika", LastName = "Kamaraj", Age = 21, Sex = "Female", Email = "monika@gmail.com", MobileNum = 8807697440, Password = "monika@@123", Status = (Role) Enum.Parse(typeof(Role), "Admin", true)});
         }
-
-        public short VerifyWithDB(string userName, string passWord)
+        public void AddUserDetails(User user)
         {
-
-            using (SqlCommand sqlCommand = new SqlCommand("spCHECKDB", sqlConnection))
-            {
-                SqlParameter sqlParameter = new SqlParameter();
-                sqlCommand.CommandType = CommandType.StoredProcedure;
-                sqlCommand.Parameters.AddWithValue("@firstname", userName);
-                sqlCommand.Parameters.AddWithValue("@password", passWord);
-                sqlCommand.Parameters.Add("@id", SqlDbType.Int, 3);
-                sqlCommand.Parameters["@id"].Direction = ParameterDirection.Output;
-                sqlConnection.Open();
-                sqlCommand.ExecuteNonQuery();
-                short ID = Convert.ToInt16(sqlCommand.Parameters["@id"].Value);
-                sqlConnection.Close();
-                return ID;
-            }
+            userDetails.Add(user);
         }
-
-        public string FetchRole(int ID)
+        public static IEnumerable<User> ShowDetails()
         {
-            using (SqlCommand sqlCommand = new SqlCommand("spFETCHROLE", sqlConnection))
-            {
-                SqlParameter sqlParameter = new SqlParameter();
-                sqlCommand.CommandType = CommandType.StoredProcedure;
-                sqlCommand.Parameters.AddWithValue("@id", ID);
-                sqlCommand.Parameters.Add("@role", SqlDbType.VarChar, 6);
-                sqlCommand.Parameters["@role"].Direction = ParameterDirection.Output;
-                sqlConnection.Open();
-                sqlCommand.ExecuteNonQuery();
-                string role = Convert.ToString(sqlCommand.Parameters["@role"].Value);
-                sqlConnection.Close();
-                return role;
-            }
+            return userDetails;
         }
-
 
     }
 }
